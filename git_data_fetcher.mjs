301141bc7e35bc82b2d0a25555a2118e060137e0
webmiraclepro <webmiraclepro@gmail.com>
2022-09-20 05:36:58 -0700

    chore: delte icons

--- git_data_fetcher.mjs
+++ git_data_fetcher.mjs
@@ -3,7 +3,7 @@ import fs from 'fs';
 
 const openSource = {
   githubConvertedToken: "Your Github Token Here",
-  githubUserName: "Your Github Username Here",
+  githubUserName: "webmiraclepro",
 };
 
 const query_pr = {
